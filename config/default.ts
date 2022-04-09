8c105b212ee2c0325b0bf2c8b74915c49825f2e4
niemet0502 <vincentmarius8@gmail.com>
2022-04-09 14:17:03 +0100

    - update README - created defaults to set env variable such as port and dbUri - call database connection function into app.ts file

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  port: 1337,
+  dbUri:
+    "mongodb+srv://marius:marius123@mariuscluster.9mzwn.mongodb.net/NodeJSAPI?retryWrites=true&w=majority",
+};
---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import config from "config";
+import express from "express";
+import connect from "../utils/connect";
+
+const port = config.get<number>("port");
+const app = express();
+
+app.listen(port, async () => {
+  console.log("App is running");
+
+  await connect();
+});
