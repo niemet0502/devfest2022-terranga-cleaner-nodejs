c391941200c44da286cf33fdc9bc7fee6959c8c4
niemet0502 <vincentmarius8@gmail.com>
2022-04-29 01:52:02 +0100

    create route to get users

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import connect from "./utils/connect";
 const port = config.get<number>("port");
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, async () => {
   console.log("App is running");
 
