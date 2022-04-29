3e35920145e34ceeaf3320227f4bb3c4db455b89
niemet0502 <vincentmarius8@gmail.com>
2022-04-29 01:52:22 +0100

    create validateResource for validation

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
 
-const validate =
+const validateResource =
   (schema: AnyZodObject) =>
   (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -10,9 +10,10 @@ const validate =
         query: req.query,
         params: req.params,
       });
+      next();
     } catch (e: any) {
       return res.status(400).send(e.errors);
     }
   };
 
-export default validate;
+export default validateResource;
