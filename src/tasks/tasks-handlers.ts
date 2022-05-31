df335cd7e9f967505c6c2b9dbacbe2b73fc0e62c
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-05-31 13:50:21 +0000

    created getTask and createTask handlers

--- src/tasks/tasks-handlers.ts
+++ src/tasks/tasks-handlers.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 
-export const createUser = (req: Request, res: Response) => {
-  res.status(201).send("create");
+export const createTask = (req: Request, res: Response) => {
+  res.status(201).send("create new task");
+};
+
+export const getTask = (req: Request, res: Response) => {
+  res.status(200).send("task list");
 };
