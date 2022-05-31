c3e7cf47203a77e46bbc9de0c2ad914e23e00bff
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-05-31 15:34:25 +0000

    created database connection then init the server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import initServer from "./initServer";
+import "reflect-metadata";
+import { AppDataSource } from "./database/data-source";
+import initServer, { createDataBaseConnection } from "./initServer";
 import routes from "./routes";
 
+createDataBaseConnection(AppDataSource);
 const app = initServer(routes);
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
