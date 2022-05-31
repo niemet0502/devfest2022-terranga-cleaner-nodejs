32c64bf6a587e515d45fdfb600252821d1ccbb5e
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-05-31 15:33:10 +0000

    created createDataBaseConnection method

--- src/initServer.ts
+++ src/initServer.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express, { Router } from "express";
+import { DataSource } from "typeorm";
 
 export default function initServer(router: Router) {
   const app = express();
@@ -10,4 +11,8 @@ export default function initServer(router: Router) {
   app.use("/v1", router);
 
   return app;
+}
+
+export async function createDataBaseConnection(AppDataSource: DataSource) {
+  await AppDataSource.initialize();
 }
