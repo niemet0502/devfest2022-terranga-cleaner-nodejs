2049002daf4ccd0c90e316f6b5f8d0084c92ec1b
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-05-31 13:49:37 +0000

    clean up the index file and used the initServer method to init the nodejs server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import cors from "cors";
-import express from "express";
-import routes from "./route";
+import initServer from "./initServer";
+import routes from "./routes";
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
-
-routes(app);
+const app = initServer(routes);
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
