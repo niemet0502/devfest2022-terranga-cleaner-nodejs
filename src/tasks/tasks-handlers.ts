97356ffbc91a3c9b3a1b4bc0411590f8148c9e30
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-07-17 13:52:53 +0000

    update task feature done !!

--- src/tasks/tasks-handlers.ts
+++ src/tasks/tasks-handlers.ts
@@ -11,17 +11,17 @@ export const createTask = async (req: Request, res: Response) => {
 };
 
 export const getTasks = async (req: Request, res: Response) => {
-  let tasks = await taskRepository.find();
+  let tasks = await taskRepository.find({ order: { id: "DESC" } });
   res.status(200).send(tasks);
 };
 
 export const updateTask = async (req: Request, res: Response) => {
-  res.status(200).send();
+  let task = await taskRepository.save(req.body);
+  res.status(200).send(task);
 };
 
 export const getTask = async (req: Request, res: Response) => {
   let id = req.params.id;
-  console.log(id);
 
   let task = await taskRepository.find({ where: { id: id } });
 
