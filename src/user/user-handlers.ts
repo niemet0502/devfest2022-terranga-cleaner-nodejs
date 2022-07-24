dc441bef1cb4f32e764c513474e8fdc77bc77baf
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-07-24 12:29:32 +0000

    added the getUsers method handler

--- src/user/user-handlers.ts
+++ src/user/user-handlers.ts
@@ -2,9 +2,15 @@ import userRepository from "./user-repositories";
 import * as userService from "./user-service";
 
 export const createUser = async (req: Request, res: Response) => {
-  let user = userService.create(req);
+  let user = await userService.create(req);
 
   user = await userRepository.save(user);
 
   res.status(200).send(user);
+};
+
+export const getUsers = async (req: Request, res: Response) => {
+  let users = await userRepository.find();
+
+  res.status(200).send(users);
 };
