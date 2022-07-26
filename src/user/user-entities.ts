736aff79b779983ac23577fc617924242359f4e1
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-07-26 19:00:21 +0000

    added OneToMany on User entity

--- src/user/user-entities.ts
+++ src/user/user-entities.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import Task from "../tasks/tasks-entities";
 
 @Entity()
 export default class User {
@@ -16,4 +17,7 @@ export default class User {
 
   @Column("text")
   password: string;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
 }
