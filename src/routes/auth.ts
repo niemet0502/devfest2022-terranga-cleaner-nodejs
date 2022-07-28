e5d364195c6b3af2eaae9db7ed17cdf29678edd8
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-07-28 17:03:14 +0000

    create an auth route for signIn

---
+++ src/routes/auth.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { signIn } from "../authentication/auth-handler";
+import { validateBody } from "../middleware/validateBody";
+import { authSchema } from "../schema/auth";
+
+const router = express.Router();
+
+router.post("/signin", validateBody(authSchema), signIn);
