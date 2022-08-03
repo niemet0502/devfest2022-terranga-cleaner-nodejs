f89620f2b3dd5b95cc35e2a4996d658915daae08
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-03 23:14:35 +0000

    fetch user while trying to sign

--- src/authentication/auth-handler.ts
+++ src/authentication/auth-handler.ts
@@ -1,3 +1,6 @@
 import { Request, Response } from "express";
+import { findUserAccount } from "./auth-service";
 
-export const signIn = (req: Request, res: Response) => {};
+export const signIn = (req: Request, res: Response) => {
+  const user = findUserAccount(req.body);
+};
