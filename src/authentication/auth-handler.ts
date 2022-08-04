79ab9cd2c7238d1067f5f6ee889e9dce18e4f06a
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-04 08:22:18 +0000

    update SignIn method
    - findUSerBYEmail
    - check if password is valid

--- src/authentication/auth-handler.ts
+++ src/authentication/auth-handler.ts
@@ -1,6 +1,19 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
-import { findUserAccount } from "./auth-service";
+import { findUserByEmail } from "../user/user-service";
 
-export const signIn = (req: Request, res: Response) => {
-  const user = findUserAccount(req.body);
+export const signIn = async (req: Request, res: Response) => {
+  const message = "Invalid email or password";
+  const { email, password } = req.body;
+  const user = await findUserByEmail(email);
+
+  if (!user) {
+    return res.send(message);
+  }
+
+  const validPassword = bcrypt.compare(password, user.password);
+
+  if (!validPassword) {
+    return res.send(message);
+  }
 };
