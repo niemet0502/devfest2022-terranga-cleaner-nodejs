8b86e28517ea8cf9c9ccc8bc1feec07e789dade8
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-06 20:45:01 +0000

    signin an user by creating a session token

--- src/authentication/auth-handler.ts
+++ src/authentication/auth-handler.ts
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { findUserByEmail } from "../user/user-service";
+import { createUserSession } from "./auth-service";
+import sessionRepository from "./session-repositories";
 
 export const signIn = async (req: Request, res: Response) => {
   const message = "Invalid email or password";
@@ -8,12 +10,17 @@ export const signIn = async (req: Request, res: Response) => {
   const user = await findUserByEmail(email);
 
   if (!user) {
-    return res.send(message);
+    return res.send({ message });
   }
 
-  const validPassword = bcrypt.compare(password, user.password);
+  const validPassword = await bcrypt.compare(password, user.password);
 
   if (!validPassword) {
-    return res.send(message);
+    return res.send({ message });
   }
+
+  const session = createUserSession(user);
+  session = await sessionRepository.save(session);
+
+  res.status(201).send(session);
 };
--- src/authentication/auth-service.ts
+++ src/authentication/auth-service.ts
@@ -3,12 +3,13 @@ import User from "../user/user-entities";
 import Session from "./session-entity";
 
 export const createUserSession = (user: User) => {
-  const accessToken = jwt.sign(user.toJSON(), "accessTokenPrivateKey");
+  const accessToken = jwt.sign(JSON.stringify(user), "accessTokenPrivateKey");
 
   const session = new Session();
 
   session.token = accessToken;
   session.user = user;
+  session.expirationDate = new Date();
 
   return session;
 };
