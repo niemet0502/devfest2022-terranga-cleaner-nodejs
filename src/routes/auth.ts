9197471d3d5691a6b9fb375abfe904e769a6c47c
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-07 09:58:59 +0000

    create signout endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { signIn } from "../authentication/auth-handler";
+import { signIn, signOut } from "../authentication/auth-handler";
+import requireUser from "../middleware/requireUser";
 import { validateBody } from "../middleware/validateBody";
 import { authSchema } from "../schema/auth";
 
 const router = express.Router();
 
 router.post("/signin", validateBody(authSchema), signIn);
+router.post("/signout", requireUser, signOut);
 
 export default router;
